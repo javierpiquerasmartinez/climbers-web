67e603a848d520cd6b57630116cbf217b25b1a60
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-03-31 13:41:53 +0200

    Actualizar MainPage: mejorar la estructura y estilos de la sección principal, ajustando el diseño y la presentación del contenido

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -23,16 +23,18 @@ export default function MainPage() {
 
   return (
     <>
-      <section className="flex flex-col max-h-max-[656px] w-full bg-center bg-cover bg-no-repeat bg-[url('/hero.webp')]">
-        <div className="bg-black/30 h-full flex flex-col justify-around items-center p-40 text-center">
-          <h1 className="text-7xl text-white font-bold">Conecta, viaja y escala</h1>
-          <p className="uppercase text-white text-2xl font-semibold">
-            Conecta con escaladores apasionados, conoce nuevos sectores de la mano de locales expertos, siéntete como en casa en sectores desconocidos y vive una aventura única.
-          </p>
-          <a className="bg-(--color-accent) px-4 py-2 rounded-2xl text-white transition transform md:hover:-translate-y-1 duration-100 ease-in-out mt-20" href="/login">Únete ya</a>
+      <section className="relative flex flex-col min-h-[656px] w-auto bg-center bg-cover bg-no-repeat bg-[url('/hero.webp')]">
+        <div className="absolute bg-black/30 justify-around items-center text-center h-full w-full flex flex-col px-10">
+          <div className="flex flex-col gap-10 max-w-3xl">
+            <h1 className="text-7xl text-white font-bold">Conecta, viaja y escala</h1>
+            <p className="uppercase text-white text-2xl font-semibold">
+              Conecta con escaladores apasionados, conoce nuevos sectores de la mano de locales expertos, siéntete como en casa en sectores desconocidos y vive una aventura única.
+            </p>
+          </div>
+          <a className="bg-(--color-accent) px-4 py-2 rounded-2xl text-white transition transform md:hover:-translate-y-1 duration-100 ease-in-out" href="/login">Únete ya</a>
         </div>
       </section>
-      <section className="flex flex-row justify-around items-center bg-(--color-accent) gap-10">
+      <section className="flex flex-row justify-around items-center bg-(--color-accent) gap-10 px-10">
         {
           items.map((item) => (
             <div key={item.id} className="flex flex-col items-center py-10 text-center gap-4">

1a47eadb984bab8f56993def7eb4a6a333025942
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-03-31 12:37:23 +0200

    Eliminar configuración de PostCSS y ajustar estilos en MainPage para mejorar la presentación

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,9 @@
 export default function MainPage() {
   return (
     <section className="h-[656px] bg-center bg-no-repeat bg-[url('/hero.webp')]">
-      <div className="bg-black/30 h-full flex flex-col justify-around items-center p-20">
+      <div className="bg-black/30 h-full flex flex-col justify-around items-center p-20 text-center">
         <h1 className="text-8xl text-white font-bold">Conecta, viaja y escala</h1>
-        <p className="uppercase text-white text-xl text-center">
+        <p className="uppercase text-white text-xl">
           Conecta con escaladores apasionados, conoce nuevos sectores de la mano de locales expertos, siéntete como en casa en sectores desconocidos y vive una aventura única.
         </p>
       </div>

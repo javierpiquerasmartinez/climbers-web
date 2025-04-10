1049885976f32a912bc8639eae0bb1ce8598bf42
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-04-10 12:45:25 +0200

    Añadir iconos de favoritos en el componente UserCard

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { User } from "../context/UserContext";
 import axiosInstance from "../lib/axios";
+import FavoriteLogo from "./icons/FavoriteLogo";
+import FavoriteLessLogo from "./icons/FavoriteLessLogo";
 
 type Props = {
   user: User;
@@ -45,13 +47,9 @@ export default function UserCard({ user, isFavorite, onToggleFavorite }: Props) 
             <>
               <button onClick={handleFavoriteClick} className="text-red-500">
                 {isFavorite ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
-                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3c3.08 0 5.5 2.42 5.5 5.5 0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-                  </svg>
+                  <FavoriteLogo />
                 ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
-                    <path d="M12 .587l3.668 7.568L24 9.167l-6,5.848L19,24l-7-3.688L5,24l1-8L0,9l8-.845z" />
-                  </svg>
+                  <FavoriteLessLogo />
                 )}
               </button></>
           }
---
+++ src/components/icons/FavoriteLessLogo.tsx
@@ -0,0 +1,7 @@
+export default function FavoriteLessLogo() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
+      <path d="M12 .587l3.668 7.568L24 9.167l-6,5.848L19,24l-7-3.688L5,24l1-8L0,9l8-.845z" />
+    </svg>
+  )
+}

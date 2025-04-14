67132ab1e528ac77080a8da7bbb3e948ef0b4334
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2025-04-14 22:51:52 +0200

    Añadir el proveedor QueryClient para integrar React Query en la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { UserProvider } from './context/UserContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+const queryClient = new QueryClient();
 
 root.render(
   <GoogleOAuthProvider clientId="513333596106-3tt5thpccc8qn7gum9nhhoinplhvdmor.apps.googleusercontent.com">
     <UserProvider>
       <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </React.StrictMode>
     </UserProvider>
   </GoogleOAuthProvider>
